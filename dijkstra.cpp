d332da284da729ff1826a80b5c0052b3545369df
geohasby <geohasby@gmail.com>
2021-06-06 06:21:44 +0700

    add the shortest path

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -1,11 +1,16 @@
+// BATASAN :
+// 1 <= vertex <= 100
+// 1 <= panjang 1 jalan (m) <= 10000
+
 #include<bits/stdc++.h>
 #define pii pair<int, int>
+#define maks 1000000
 using namespace std;
 
-int main(){
-	//BATASAN MAKSIMAL ADA 100 VERTEX ATAU TITIK ATAU TEMPAT
+int main(){ 
 	int v, e, x, y, z, a, b, weight;
 	int dist[105];			//untuk menampung jarak terpendek dari vertex asal ke masing-masing vertex lainnya
+	string path[105];		//untuk menampung lintasan / jalur terpendex dari vertex asal ke masing-masing vertex lainnya
 	vector< pii >adj[105];	//adjacency list
 	priority_queue< pii , vector< pii > , greater< pii > >pq;	//priority queue atau min heap yang digunakan untuk algo dijkstra
 	
@@ -15,7 +20,7 @@ int main(){
 	cout<<"Masukkan banyak edge : ";
 	cin>>e;
 	
-	cout<<"Masukkan keterangan untuk "<<e<<" jalan (titik asal, titik tujuan, jarak) : "<<endl;
+	cout<<"Masukkan keterangan untuk "<<e<<" jalan (vertex asal, vertex tujuan, jarak) : "<<endl;
 	for(int i=0; i<e; i++){
 		cin>>x>>y>>z;		//jalan menghubungkan kota x ke y dan jaraknya z
 		
@@ -24,19 +29,20 @@ int main(){
 		adj[y].push_back(make_pair(x,z));
 	}
 	
-	cout<<"Masukkan titik awal : ";
+	cout<<"Masukkan vertex awal : ";
 	cin>>a;
 	
-	cout<<"Masukkan titik tujuan : ";
+	cout<<"Masukkan vertex tujuan : ";
 	cin>>b;
 	
 	//set distance pada semua vertex menjadi infinite
-	for(int i=0; i<v; i++)
-		dist[i] = INT_MAX;
-	
-	//insert titik awal dan jarak awalnya masih 0
+	for(int i=1; i<=v; i++)
+		dist[i] = maks;
+		
+	//insert vertex awal dan jarak awalnya masih 0
 	pq.push(make_pair(0,a));
 	dist[a] = 0;
+	path[a] = char(a+48);
 	
 	//looping sampai isi priority queue habis / kosong
 	while(!pq.empty()){
@@ -52,12 +58,16 @@ int main(){
 			//jika jarak dari x ke y lebih kecil daripada jarak yang sudah ada di y, maka update nilainya
 			if(dist[y] > dist[x] + weight){
 				dist[y] = dist[x] + weight;
+				path[y] = path[x] + '-' + char(y+48);
 				pq.push(make_pair(dist[y],y));
 			}
 		}
 	}
 	
-	cout<<dist[b]<<endl;	//outputkan jarak terpendek untuk menuju ke vertex tujuan
+	//outputkan jarak terpendek untuk menuju ke vertex tujuan
+	if(dist[b] == maks)	//jika jarak pada vertex tujuan = infinite (berarti tidak ditemukan rute)
+		cout<<"Tidak ditemukan rute dari vertex "<<a<<" ke vertex "<<b<<endl;
+	else cout<<"Jarak terpendek = "<<dist[b]<<" m"<<endl<<"Rute : "<<path[b]<<endl;
 }
 
 //REFERENSI : https://www.geeksforgeeks.org/dijkstras-shortest-path-algorithm-using-priority_queue-stl/
