8b3aa730e96cc1b2aede6393a7ccc32d8e83588c
geohasby <geohasby@gmail.com>
2021-06-08 22:36:09 +0700

    add fastest route

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -1,44 +1,56 @@
+//vertex max 100 -> 0-9
+//panjang jalan (km)
+
 // BATASAN :
-// 1 <= vertex <= 100
+// 1 <= vertex <= 100 (zero-index)
 // vertex-1 <= edge <= vertex*(vertex-1)/2
-// 1 <= panjang 1 jalan (m) <= 10.000
+// 1 <= panjang jalan (km) <= 100
 // 1 <= kendaraan umum <= 100
 // 1 <= ongkos kendaraan umum <= 100.000
 // 1 <= kecepatan (km/jam) <= 100
 // headway (menit) : faktor dari 60 (1, 2, 3, 4, 5, 6, 10, 12, 15, 20, 30, 60) 
 
 #include<bits/stdc++.h>
-#include<string>
 #define pii pair<int, int>
 #define maks INT_MAX
 using namespace std;
 
 struct transp{
 	string name;
-	int fare;
 	int speed;
 	int headway;
 };
 
-int v, e, x, y, z, pt, a, b, d, hw, spd, mnt, wt, weight, jam, menit, vhc_now;
-string circuit;
-int prev[105];					//menyimpan informasi indeks vertex yang dilalui sebelumnya
-transp pub_transp[105];			//menyimpan informasi kendaraan umum (nama, ongkos, kecepatan, headway)
-vector< int >option[105][105];	//pilihan kendaraan yang bisa digunakan untuk masing-masing pasangan vertex	
-vector< pii >adj[105];			//adjacency list
-priority_queue< pii , vector< pii > , greater< pii > >pq;	//priority queue atau min heap yang digunakan untuk algoritma dijkstra
-int dist[105];					//menampung jarak terpendek dari vertex asal ke masing-masing vertex lainnya
-int cost[105];					//menampung biaya termurah dari vertex asal ke masing-masing vertex lainnya
-int travel_time[105];			//menampung waktu tempuh tercepat (detik) dari vertex asal ke masing-masing vertex lainnya
+struct prv{
+	int vertex;
+	int waiting_time;
+	int vehicle;
+};
 
-string route(int now, string s){
-	if(prev[now] == now)
-		return char(now+48) + s;
+int v, e, x, y, z, pt, a, b, d, temp, hw, spd, mnt, wt, weight, jam, menit, vhc_now, prev_vhc;
+int dist[105], travel_time[105];	//menyimpan jarak terpendek, waktu tempuh tercepat dari vertex asal ke masing-masing vertex lainnya
+prv prev[105];						//menyimpan informasi indeks vertex yang dilalui sebelumnya
+transp pub_transp[105];				//menyimpan informasi kendaraan umum (nama, ongkos, kecepatan, headway)
+vector< int >option[105][105];		//pilihan kendaraan yang bisa digunakan untuk masing-masing pasangan vertex	
+vector< pii >adj[105];				//adjacency list
+priority_queue< pii , vector< pii > , greater< pii > >pq;	//priority queue atau min heap yang digunakan untuk algoritma dijkstra
+
+void shortest_route(int now){
+	if(prev[now].vertex == now)
+		cout<<now;
 	else{
-		s = char(now+48) + s;
-		s = '-' + s;
-		return route (prev[now], s);	
+		shortest_route(prev[now].vertex);
+		cout<<" - "<<now;
 	}
+	return;
+}
+
+void fastest_route(int now){
+	if(prev[now].vertex != now){
+		fastest_route(prev[now].vertex);
+		cout<<"\n\tdari titik "<<prev[now].vertex<<" menunggu "<<prev[now].waiting_time / 60<<" menit "<<prev[now].waiting_time % 60<<" detik, kemudian naik "<<pub_transp[prev[now].vehicle].name<<" ke titik "<<now;
+	}
+	return;
 }
 
 void find_shortest_path(){
@@ -49,7 +61,7 @@ void find_shortest_path(){
 	//insert vertex awal dan jarak awalnya masih 0
 	pq.push(make_pair(0,a));
 	dist[a] = 0;
-	prev[a] = a;
+	prev[a].vertex = a;
 	
 	//looping sampai isi priority queue habis / kosong
 	while(!pq.empty()){
@@ -65,7 +77,7 @@ void find_shortest_path(){
 			//jika jarak dari x ke y lebih kecil daripada jarak yang sudah ada di y, maka update nilainya
 			if(dist[y] > dist[x] + weight){
 				dist[y] = dist[x] + weight;
-				prev[y] = x;
+				prev[y].vertex = x;
 				pq.push(make_pair(dist[y],y));
 			}
 		}
@@ -75,7 +87,11 @@ void find_shortest_path(){
 	//outputkan jarak terpendek untuk menuju ke vertex tujuan
 	if(dist[b] == maks)	//jika jarak pada vertex tujuan = infinite (berarti tidak ditemukan rute)
 		cout<<" - Tidak ditemukan rute dari vertex "<<a<<" ke vertex "<<b<<endl;
-	else cout<<" - Jarak terpendek = "<<dist[b]<<" m"<<endl<<" - Rute : "<<route(b, "")<<endl;
+	else{
+		cout<<" - Jarak terpendek = "<<dist[b]<<" m"<<endl<<" - Rute : ";
+		shortest_route(b);
+		cout<<endl;
+	}
 }
 
 void find_cheapest_path(){
@@ -133,9 +149,9 @@ void find_fastest_path(){
 		travel_time[i] = maks;
 	
 	//anggap keberangkatan dimulai dari menit 00
-	pq.push(make_pair(menit,a));	//insert vertex awal
+	pq.push(make_pair(menit*60,a));	//insert vertex awal
 	travel_time[a] = menit*60;		//waktu (detik) yang sudah ditempuh sama dengan menit keberangkatan * 60
-	prev[a] = a;
+	prev[a].vertex = a;
 	
 	//looping sampai isi priority queue habis / kosong
 	while(!pq.empty()){
@@ -146,31 +162,31 @@ void find_fastest_path(){
 		//cek semua tetangga dari vertex x
 		for(int i=0; i<adj[x].size(); i++){
 			y = adj[x][i].first;			//index vertex tetangga
-			d = adj[x][i].second;			//jarak dari vertex x ke vertex tetangga ke-i
+			d = adj[x][i].second;			//waktu tempuh dari vertex x menuju vertex tetangga ke-i
 			
-			//cek semua kemungkinan kendaraan umum yang tersedia dari vertex x ke y
+			//cek semua transportasi umum yang tersedia dari vertex x ke y
 			for(int j=0; j<option[x][y].size(); j++){
-				
 				vhc_now = option[x][y][j];				//kendaraan yang (mungkin) akan digunakan sekarang
 				hw = pub_transp[vhc_now].headway*60;	//headway kendaraan (detik)
 				spd = pub_transp[vhc_now].speed;		//kecepatan kendaraan
 				spd = spd*1000/3600;					//convert dari km/h ke m/s
-				
 				mnt = travel_time[x] % 3600;			//waktu yang sudah ditempuh (menitnya saja (dalam satuan detik))
 				
 				//waiting time
 				if(mnt % hw == 0) wt = 0;		//tidak perlu menunggu
-				else wt = hw - (mnt % hw);		//waktu tunggu (menit)
+				else wt = hw - (mnt % hw);		//waktu tunggu (detik)
 				
 				weight = wt + d/spd;			//total waktu yang dibutuhkan (detik) untuk menuju ke vertex y dengan kendaraan tersebut
 				
 				//jika waktu tempuh dari x ke y lebih cepat daripada waktu yang sudah ada di y, maka update nilainya
 				if(travel_time[y] > travel_time[x] + weight){
 					travel_time[y] = travel_time[x] + weight;
-					prev[y] = x;
+					prev[y].vertex = x;
+					prev[y].waiting_time = wt;
+					prev[y].vehicle = vhc_now;
 					pq.push(make_pair(travel_time[y],y));
 //					cout<<x<<' '<<y<<' '<<vhc_now<<' '<<travel_time[y]-travel_time[a]<<" ("<<weight<<' '<<d/spd<<' '<<wt<<')'<<endl;
-				}			
+				}
 			}
 		}
 	}
@@ -179,39 +195,48 @@ void find_fastest_path(){
 	//outputkan jarak terpendek untuk menuju ke vertex tujuan
 	if(travel_time[b] == maks)	//jika jarak pada vertex tujuan = infinite (berarti tidak ditemukan rute)
 		cout<<" - Tidak ditemukan rute dari vertex "<<a<<" ke vertex "<<b<<endl;
-	else cout<<" - Waktu tempuh tercepat = "<<(travel_time[b] - travel_time[a]) / 60<<" menit"<<endl<<" - Rute : "<<route(b, "")<<endl;	
+	else{
+		cout<<" - Waktu tempuh tercepat = "<<(travel_time[b] - travel_time[a]) / 60<<" menit"<<endl<<" - Rute : ";
+		fastest_route(b);
+		cout<<endl;
+	} 	
 }
 
 int main(){
-	cout<<"Masukkan banyak vertex : ";
+	cout<<"Masukkan banyak titik / tempat : ";
 	cin>>v;
 	
-	cout<<"Masukkan banyak edge : ";
+	cout<<"Masukkan banyak jalan : ";
 	cin>>e;
 	
-	cout<<"Masukkan keterangan untuk "<<e<<" jalan (vertex asal, vertex tujuan, jarak) : "<<endl;
+	cout<<"Masukkan keterangan untuk masing-masing jalan : "<<endl;
+	cout<<"(titik asal, titik tujuan, jarak)"<<endl;
 	for(int i=0; i<e; i++){
-		cin>>x>>y>>z;		//jalan menghubungkan kota x ke y dan jaraknya z
+		cin>>x>>y>>z;		//jalan menghubungkan titik x ke y dan jaraknya z
 		
 		//masukkan datanya ke adjacency list
 		adj[x].push_back(make_pair(y,z));
 		adj[y].push_back(make_pair(x,z));
 	}
 	
-	cout<<"Masukkan banyak kendaraan umum : ";
+	cout<<"Masukkan banyak pilihan transportasi umum : ";
 	cin>>pt;
 	
-	cout<<"Masukkan keterangan untuk "<<pt<<" kendaran umum (nama, ongkos, kecepatan, headway, dan rute(titik awal = titik akhir) ) : "<<endl;
+	cout<<"Masukkan keterangan untuk masing-masing jenis transportasi umum : "<<endl;
+	cout<<"(nama, kecepatan (km/h), headway (menit), jumlah titik yang dilalui, dan titik mana saja yang dilalui)"<<endl;
 	for(int i=0; i<pt; i++){
-		cin>>pub_transp[i].name>>pub_transp[i].fare>>pub_transp[i].speed>>pub_transp[i].headway>>circuit;
+		cin>>pub_transp[i].name>>pub_transp[i].speed>>pub_transp[i].headway>>z;
 		
-		//masukkan indeks kendaraan tersebut ke array opsi kendaraan umum
-		//untuk setiap jalan yang dilalui rute kendaraan tersebut
-		for(int j=0; j<circuit.length()-1; j++){
-			x = int(circuit[j]) - 48;
-			y = int(circuit[j+1]) - 48;
+		//menerima inputan titik / tempat mana saja yang dilewati oleh rute kendaraan ke-i
+		//lalu menambahkan kendaraan ke-i kedalam array opsi kendaraan pada setiap ruas jalan yang dilaluinya
+		cin>>temp;
+		x = temp;
+		for(int j=1; j<z; j++){
+			cin>>y;
 			option[x][y].push_back(i);
+			x = y;
 		}
+		option[y][temp].push_back(i);
 	}
 	
 	cout<<"Masukkan vertex awal : ";
